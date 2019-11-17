aa3aab8cc5fe4fd57e441a408f70f294261cafea
Jerome Tang <JeromeTang@139.com>
2019-11-17 18:16:37 +0800

    updates

--- src/java/com/jtang/alg/sort/HeapSort.java
+++ src/java/com/jtang/alg/sort/HeapSort.java
@@ -30,23 +30,26 @@ public class HeapSort {
      * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
      *
      * @param arr
-     * @param i
+     * @param currentIndex
      * @param length
      */
-    public static void adjustHeap(int[] arr, int i, int length) {
-        int temp = arr[i];//先取出当前元素i
-        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {//从i结点的左子结点开始，也就是2i+1处开始
-            if (k + 1 < length && arr[k] < arr[k + 1]) {//如果左子结点小于右子结点，k指向右子结点
+    public static void adjustHeap(int[] arr, int currentIndex, int length) {
+        int temp = arr[currentIndex];//先取出当前元素i
+        //从i结点的左子结点开始，也就是2i+1处开始
+        for (int k = currentIndex * 2 + 1; k < length; k = k * 2 + 1) {
+            //左右比较：如果左子结点小于右子结点，k指向右子结点
+            if (k + 1 < length && arr[k] < arr[k + 1]) {
                 k++;
             }
-            if (arr[k] > temp) {//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
-                arr[i] = arr[k];
-                i = k;
+            //上下比较： 如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+            if (arr[k] > temp) {
+                arr[currentIndex] = arr[k];
+                currentIndex = k;
             } else {
                 break;
             }
         }
-        arr[i] = temp;//将temp值放到最终的位置
+        arr[currentIndex] = temp;//将temp值放到最终的位置
     }
 
     /**
---
+++ src/java/com/jtang/alg/sort/QuickSort_Start.java
@@ -0,0 +1,15 @@
+package com.jtang.alg.sort;
+
+public class QuickSort_Start {
+
+
+    public static void quickSort(int[] arr, int low, int high){
+        if (low >= high){
+            return;
+        }
+        int base = arr[low];
+        int i = low;
+        int j = high;
+
+    }
+}
