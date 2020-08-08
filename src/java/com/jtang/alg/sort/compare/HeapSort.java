f0274f19b0e634beda39a702d19d7569d40dc31a
Jerome Tang <Jerome.Tang@quest.com>
2020-08-08 10:29:12 +0800

    comments for HeapSort.java

--- src/java/com/jtang/alg/sort/compare/HeapSort.java
+++ src/java/com/jtang/alg/sort/compare/HeapSort.java
@@ -7,7 +7,8 @@ import java.util.Arrays;
  */
 public class HeapSort {
     public static void main(String[] args) {
-        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+//        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] arr = {6, 7, 8, 9, 5, 4, 3, 2, 1};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -15,13 +16,27 @@ public class HeapSort {
     public static void sort(int[] arr) {
         //1.构建大顶堆
         for (int i = arr.length / 2 - 1; i >= 0; i--) {
-            //从第一个非叶子结点从下至上，从右至左调整结构
+            /**
+             * 从第一个非叶子结点从下至上，从右至左调整结构
+             * 相当于局部调整
+             */
             adjustHeap(arr, i, arr.length);
+            System.out.println(Arrays.toString(arr));
         }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Reordering...");
         //2.调整堆结构+交换堆顶元素与末尾元素
         for (int j = arr.length - 1; j > 0; j--) {
-            swap(arr, 0, j);//将堆顶元素与末尾元素进行交换
+            /**
+             * 基于已有的大顶堆，
+             * 2.1, 将堆顶元素与末尾元素进行交换, 相当于每次选出最大值放在最后
+             */
+            swap(arr, 0, j);
+            /**
+             * 2.2，将剩下的元素重新生成大顶堆，也就是找到最大元素放到堆顶
+             */
             adjustHeap(arr, 0, j);//重新对堆进行调整
+            System.out.println(Arrays.toString(arr));
         }
 
     }
