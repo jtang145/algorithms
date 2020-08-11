0404e78f1810e0d29fbf1e8cf9751630f1d47210
Jerome Tang <Jerome.Tang@quest.com>
2020-08-11 12:43:24 +0800

    update comments

--- src/java/com/jtang/alg/sort/compare/QuickSort.java
+++ src/java/com/jtang/alg/sort/compare/QuickSort.java
@@ -14,6 +14,18 @@ public class QuickSort {
         qsort(arr, 0, arr.length - 1);
     }
 
+    /**
+     *  采用分治思想，通过不断划分大小区间来实现排序。
+     *  对于数组区间，通过以下步骤确定一个“基点”位置：
+     *  1，分配2个指针，随机选择头部”基点“数作为指针1，再把尾部作为指针2，
+     *  2. 从另一边开始比较，大数不变化，小数与首部元素交换，
+     *  3. 再从头部开始，小数不变化，大数与尾部元素交换，
+     *  4. 不断重复2,3步，直到指针1>=指针2，把“基点”数，赋值给指针1，
+     *  划分为新的2个子区间，重复在子区间执行排序
+     * @param arr
+     * @param low
+     * @param high
+     */
     private static void qsort(int[] arr, int low, int high) {
         if (low < high) {
             int pivot = partition(arr, low, high);        //将数组分为两部分
--- src/java/com/jtang/alg/sort/compare/QuickSort_Progress.java
+++ src/java/com/jtang/alg/sort/compare/QuickSort_Progress.java
@@ -2,11 +2,12 @@ package com.jtang.alg.sort.compare;
 
 import java.util.Arrays;
 
-public class QuickSort_Exe extends SwapAction {
+public class QuickSort_Progress extends SwapAction {
 
     public static void main(String[] args) {
 //        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
         int[] arr = {4, 5, 7, 8, 1, 9, 2, 3, 6};
+        System.out.println(Arrays.toString(arr));
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
@@ -16,12 +17,12 @@ public class QuickSort_Exe extends SwapAction {
             return;
         }
         int base = arr[low];
-        System.out.println(String.format("|=> base is %s, sorting from %s to %s", base, low, high));
+        System.out.println(String.format("|=> base is %s, sorting from index: %s to %s", base, low, high));
         int i = low;
         int j = high;
         while (i < j) {
             while (arr[j] >= base && i < j) {
-                System.out.println(String.format("checked %s and move to %s", arr[j], arr[j - 1]));
+                System.out.println(String.format("checked %s and moving to index %s", arr[j], (j - 1)));
                 j--;
             }
             if (i < j) {
@@ -30,12 +31,14 @@ public class QuickSort_Exe extends SwapAction {
             }
 
             while (arr[i] <= base && i < j) {
-                System.out.println(String.format("checked %s and move to %s", arr[i], arr[i + 1]));
+                System.out.println(String.format("checked %s and moving to index %s", arr[i], i + 1));
                 i++;
             }
             arr[j] = arr[i];
+            System.out.println(String.format("switching %s to index %s of value %s", arr[i], j, arr[j]));
         }
         arr[i] = base;
+        System.out.println(String.format("+++ fixed value %s to index %s", base, i));
         quickSort(arr, low, i - 1);
         quickSort(arr, i + 1, high);
     }
